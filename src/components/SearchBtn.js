41deb46c2d6b86e84cb4cd4797144d77003432d6
Maher El Gamil <maherbusnes@gmail.com>
2022-04-17 05:23:35 +0200

    link search page with home page and reverse

--- src/components/SearchBtn.js
+++ src/components/SearchBtn.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
+import { Link } from "react-router-dom";
 
 export default class SearchBtn extends Component {
     render() {
         return (
             <div className="open-search">
-                <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+                <Link
+                    to="/search"
+                >Add a book</Link>
             </div>
         );
     }
